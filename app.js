2383c371edc98195ba6f531e0a255455a17a9baa
help <141429309+goldenboots69@users.noreply.github.com>
2023-08-16 17:51:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
   const errorPopup = document.getElementById('errorPopup');
   const closePopupBtn = document.getElementById('closePopupBtn');
 
-  errorPopup.style.display = 'block'; // Show the popup when the page loads
+  
+
+  // Initially hide the popup
+  errorPopup.style.display = 'none';
 
   closePopupBtn.addEventListener('click', () => {
     errorPopup.style.display = 'none';
