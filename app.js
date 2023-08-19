37d8845364512a69f5e6afb6ee8096f56cf40ce7
help <141429309+goldenboots69@users.noreply.github.com>
2023-08-19 20:06:15 +0530

    app.js

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const errorPopup = document.getElementById('errorPopup');
   const closePopupBtn = document.getElementById('closePopupBtn');
 
-
-    errorPopup.style.display = "flex";
+  errorPopup.style.display = 'block'; // Show the popup when the page loads
 
   closePopupBtn.addEventListener('click', () => {
     errorPopup.style.display = 'none';
